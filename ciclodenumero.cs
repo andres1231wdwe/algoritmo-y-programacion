cb019b70dae28f95622244f464252bfa72ecd022
Andres Fernandez <andresestiben4@gmail.com>
2026-09-22 11:15:25 -0500

    Add Main method to print numbers from 1 to 100
    22/9/2026

---
+++ ciclodenumero.cs
@@ -0,0 +1,10 @@
+public static void Main(string[] args)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.WriteLine();
+            Console.WriteLine(i);
+
+        }
+    }
+}
